1e4265baa34d142af0753455aac9b9409069d3c5
grnvs <c.eschkoetter@web.de>
2019-11-23 17:10:05 +0100

    jo

--- app/src/main/java/org/bingmaps/app/MainActivity.java
+++ app/src/main/java/org/bingmaps/app/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,6 +22,12 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ViewFlipper;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.bingmaps.sdk.BingMapsView;
 import org.bingmaps.sdk.Coordinate;
@@ -42,6 +49,7 @@ import java.util.List;
 import static org.bingmaps.app.Constants.PERMISSION_LOCATION_REQUEST_CODE;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     @SuppressWarnings("unused")
     private final MapMovedListener mapMovedListener = new MapMovedListener() {
         public void onAvailableChecked() {
@@ -135,8 +143,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Initialize();
+        basicReadWrite();
     }
+    public void basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
+        myRef.setValue("Hello, Lorenzo!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // [END read_message]
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == PERMISSION_LOCATION_REQUEST_CODE) {
